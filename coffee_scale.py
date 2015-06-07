03fe5b56153ff3c491923b8a5f77998da9860d5f
James Alexander <jalexander@leafsoftwaresolutions.com>
2015-06-07 12:52:41 -0400

    - Refactoring coffee_scale script to be encapsulated within a class. - Fixed unit tests accordingly. - Moved Configuration variables into cached property declarations. Previously, when attempting to log a failed attempt to read a variable, the logging variable was not set. This ensures that the logging class is setup prior to any access of the environment variables.

--- coffee_scale.py
+++ coffee_scale.py
@@ -14,185 +14,206 @@ import math
 import requests
 import json
 
-logger = logging.getLogger("coffee_log")
-logger.setLevel(logging.INFO)
 
-_currentWeight = 0
-_weightChangedThreshold = 5
-_emptyPotThreshold = 10
-_loopCount = 0
-_logToHipChatLoopCount = 40
-_mugAmounts = [1200, 1466, 1732, 1998, 2264, 2530]
-_mugFluidCapacity = 266
+class CoffeeScale:
+    def __init__(self):
+        self._logger = logging.getLogger("coffee_log")
+        self._logger.setLevel(logging.INFO)
+        self._currentWeight = 0
+        self._weightChangedThreshold = 5
+        self._emptyPotThreshold = 10
+        self._loopCount = 0
+        self._logToHipChatLoopCount = 40
+        self._mugAmounts = [1200, 1466, 1732, 1998, 2264, 2530]
+        self._mugFluidCapacity = 266
+        self._initialStateKey = ''
+        self._environment = ''
+        self._hipchatKey = ''
+        self._ledServiceUrl = ''
 
-def getParser():
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('tempFile', help='Temporary output location file to write', 
-            default='/var/tmp/coffee_scale')
-    parser.add_argument('permanentDirectory', help='Permanent storage location for scale data')
-    parser.add_argument('logRotateTimeMinutes', 
-            help='Number of minutes to capture data in the temp-file before writing to the permanent directory',
-            type=int)
+    @property
+    def initialStateKey(self):
+        if not self._initialStateKey:
+            self._initialStateKey = os.environ.get('INITIAL_STATE_ACCESS_KEY')
+            if not self._initialStateKey:
+                self._logger.error("### Initial State Key not set in environment variable")
 
-    return parser
+        return self._initialStateKey
 
-if __name__ == "__main__":
-    parser = getParser()
-    args = parser.parse_args()
-    handler = TimedRotatingFileHandler(args.tempFile,
-            when="m", interval=args.logRotateTimeMinutes, utc=True)
+    @property
+    def environment(self):
+        if not self._environment:
+            self._environment = os.environ.get("ENVIRONMENT")
+            if not self._environment:
+                self._environment = "prod"
 
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+        return self._environment
 
-_initialStateKey = os.environ.get('INITIAL_STATE_ACCESS_KEY')
-if not _initialStateKey:
-    logger.error("### Initial State Key not set in environment variable")
+    @property
+    def hipchatKey(self):
+        if not self._hipchatKey:
+            self._hipchatKey = os.environ.get('HIPCHAT_KEY')
+            if not self._hipchatKey:
+                self._logger.error('### Hipchat API Key missing from environment variable HIPCHAT_KEY')
 
-_environment = os.environ.get("ENVIRONMENT")
-if not _environment:
-    _environment = "prod"
+        return self._hipchatKey
 
-_hipchatKey = os.environ.get('HIPCHAT_KEY')
-if not _hipchatKey:
-    logger.error('### Hipchat API Key missing from environment variable HIPCHAT_KEY')
+    @property
+    def ledServiceUrl(self):
+        if not self._ledServiceUrl:
+            self._ledServiceUrl = os.environ.get('LED_SERVICE_URL')
+            if not self._ledServiceUrl:
+                self._logger.error('### LED_SERVICE_URL environment variable has not been set') 
 
-_ledServiceUrl = os.environ.get('LED_SERVICE_URL')
-if not _ledServiceUrl:
-   logger.error('### LED_SERVICE_URL environment variable has not been set') 
+        return self._ledServiceUrl
 
-def getWeightInGrams(dev="/dev/usb/hiddev0"):
-    """
-    This device normally appears on /dev/usb/hiddev0, assume
-    device still appears on this file handle.
-    """
-    # If we cannot find the USB device, return -1
+    def getParser(self):
+        import argparse
+        parser = argparse.ArgumentParser()
+        parser.add_argument('tempFile', help='Temporary output location file to write', 
+                default='/var/tmp/coffee_scale')
+        parser.add_argument('permanentDirectory', help='Permanent storage location for scale data')
+        parser.add_argument('logRotateTimeMinutes', 
+                help='Number of minutes to capture data in the temp-file before writing to the permanent directory',
+                type=int)
 
-    grams = -1
-    try:
-        fd = os.open(dev, os.O_RDONLY)
+        return parser
 
-        # Read 4 unsigned integers from USB device
-        hiddev_event_fmt = "IIII"
-        usb_binary_read = struct.unpack(hiddev_event_fmt, os.read(fd, struct.calcsize(hiddev_event_fmt)))
-        grams = usb_binary_read[3]
-        os.close(fd)
-    except OSError as e:
-        print("{0} - Failed to read from USB device".format(datetime.utcnow()))
-    return grams
+    def configureLogFile(self, args):
+        handler = TimedRotatingFileHandler(args.tempFile,
+                when="m", interval=args.logRotateTimeMinutes, utc=True)
 
-def moveLogsToArchive(tempFilePath, archiveDir):
-    """
-    Using shutil.move here since the raspberry pi will 
-    be moving the files from a tempfs file system to the
-    ext3 file system on the SD card. 
-    """
-    tempFileName = os.path.basename(tempFilePath)
-    tempFileDir = os.path.dirname(tempFilePath)
-    logFiles = glob.glob("{0}.*".format(tempFilePath))
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        handler.setFormatter(formatter)
+        self._logger.addHandler(handler)
 
-    for fileName in logFiles:
-        shutil.move(fileName, os.path.join(archiveDir, os.path.basename(fileName)))
+    def shouldLogWeight(self, newReading):
+        return abs(self._currentWeight - newReading) > self._weightChangedThreshold
 
-def shouldLogWeight(newReading):
-    return abs(_currentWeight - newReading) > _weightChangedThreshold
+    def potIsLifted(self):
+        return self._currentWeight <= self._emptyPotThreshold
 
-def potIsLifted():
-    return _currentWeight <= _emptyPotThreshold
+    def shouldPostToHipChat(self):
+        return self._loopCount == self._logToHipChatLoopCount
 
-def shouldPostToHipChat():
-    return _loopCount == _logToHipChatLoopCount
+    def getHipchatParameters(self):
+        parameters = {}
+        # Fridge Room
+        parameters['room_id'] = 926556
+        totalAvailableMugs = len(self._mugAmounts)
+        parameters['from'] = "{0} / {1}".format(self.getAvailableMugs(), totalAvailableMugs)
+        parameters['message'] = "{0} / {1}".format(self._currentWeight, self._mugAmounts[totalAvailableMugs - 1]) 
+        parameters['color'] = 'random'
 
-def shouldPostToLed():
-    return _loopCount == _logToHipChatLoopCount
+        return parameters
 
-def postToLed():
-    displayJson = {}
-    totalAvailableMugs = len(_mugAmounts)
-    displayJson['text'] = "{0} / {1} - {2} / {3}".format(getAvailableMugs(), totalAvailableMugs,
-            _currentWeight, _mugAmounts[totalAvailableMugs - 1])
+    def getAvailableMugs(self):
+        availableMugs = 0
+        for mugAmount in self._mugAmounts:
+            minimumWeightForMug = math.floor(mugAmount - (self._mugFluidCapacity * .1)) - 10
+            if self._currentWeight <= minimumWeightForMug:
+                break
+            availableMugs += 1
 
-    url = "{0}/display".format(_ledServiceUrl)
-    payload = json.dumps(displayJson)
-    headers = {'content-type': 'application/json'}
+        return availableMugs
+            
 
-    # TODO: Identify why the this code is throwing the following exception:
-    # requests.exceptions.ConnectionError: ('Connection aborted.', BadStatusLine('HTTP/1.1 1 \r\n',))
-    try:
+    def getWeightInGrams(self, dev="/dev/usb/hiddev0"):
+        """
+        This device normally appears on /dev/usb/hiddev0, assume
+        device still appears on this file handle.
+        """
+        # If we cannot find the USB device, return -1
+
+        grams = -1
+        try:
+            fd = os.open(dev, os.O_RDONLY)
+
+            # Read 4 unsigned integers from USB device
+            hiddev_event_fmt = "IIII"
+            usb_binary_read = struct.unpack(hiddev_event_fmt, os.read(fd, struct.calcsize(hiddev_event_fmt)))
+            grams = usb_binary_read[3]
+            os.close(fd)
+        except OSError as e:
+            print("{0} - Failed to read from USB device".format(datetime.utcnow()))
+        return grams
+
+    def moveLogsToArchive(self, tempFilePath, archiveDir):
+        """
+        Using shutil.move here since the raspberry pi will 
+        be moving the files from a tempfs file system to the
+        ext3 file system on the SD card. 
+        """
+        tempFileName = os.path.basename(tempFilePath)
+        tempFileDir = os.path.dirname(tempFilePath)
+        logFiles = glob.glob("{0}.*".format(tempFilePath))
+
+        for fileName in logFiles:
+            shutil.move(fileName, os.path.join(archiveDir, os.path.basename(fileName)))
+
+    def shouldPostToLed(self):
+        return self_loopCount == self._logToHipChatLoopCount
+
+    def postToLed(self):
+        displayJson = {}
+        totalAvailableMugs = len(self._mugAmounts)
+        displayJson['text'] = "{0} / {1} - {2} / {3}".format(self.getAvailableMugs(), totalAvailableMugs,
+                _currentWeight, _mugAmounts[totalAvailableMugs - 1])
+
+        url = "{0}/display".format(self.ledServiceUrl)
+        payload = json.dumps(displayJson)
+        headers = {'content-type': 'application/json'}
+
         response = requests.post(url, data=payload, headers=headers)
-    except:
-        pass
 
-def logToInitialState():
-    utcnow = datetime.utcnow()
-    bucketKey = "{0} - coffee_scale_data".format(_environment)
+    def logToInitialState(self):
+        utcnow = datetime.utcnow()
+        bucketKey = "{0} - coffee_scale_data".format(self.environment)
 
-    streamer = Streamer(bucket_name="{0} - Coffee Scale Data".format(_environment), 
-            bucket_key=bucketKey, access_key=_initialStateKey)
+        streamer = Streamer(bucket_name="{0} - Coffee Scale Data".format(self.environment), 
+                bucket_key=bucketKey, access_key=self.initialStateKey)
 
-    if potIsLifted():
-        streamer.log("Coffee Pot Lifted", True)
-    streamer.log("Coffee Weight", _currentWeight)
-    streamer.close()
+        if potIsLifted():
+            streamer.log("Coffee Pot Lifted", True)
+        streamer.log("Coffee Weight", self._currentWeight)
+        streamer.close()
 
-def getHipchatParameters():
-    parameters = {}
-    # Fridge Room
-    parameters['room_id'] = 926556
-    totalAvailableMugs = len(_mugAmounts)
-    parameters['from'] = "{0} / {1}".format(getAvailableMugs(), totalAvailableMugs)
-    parameters['message'] = "{0} / {1}".format(_currentWeight, _mugAmounts[totalAvailableMugs - 1]) 
-    parameters['color'] = 'random'
+    def writeToHipChat(self):
+        hipster = hipchat.HipChat(token=self.hipchatKey)
+        params = getHipchatParameters()
+        hipster.method('rooms/message', method='POST', parameters=params)
 
-    return parameters
+    def main(self, args):
+        rotateMinutes = timedelta(minutes = args.logRotateTimeMinutes)
+        rotateTime = datetime.utcnow() + rotateMinutes
+        self._currentWeight = self.getWeightInGrams()
 
-def writeToHipChat():
-    hipster = hipchat.HipChat(token=_hipchatKey)
-    params = getHipchatParameters()
-    hipster.method('rooms/message', method='POST', parameters=params)
+        while True:
+            self._loopCount += 1
+            tmpWeight = self.getWeightInGrams()
+            if datetime.utcnow() > rotateTime:
+                self.moveLogsToArchive(args.tempFile, args.permanentDirectory)
+                rotateTime = datetime.utcnow() + rotateMinutes
 
-def getAvailableMugs():
-    availableMugs = 0
-    for mugAmount in _mugAmounts:
-        minimumWeightForMug = math.floor(mugAmount - (_mugFluidCapacity * .1)) - 10
-        if _currentWeight <= minimumWeightForMug:
-            break
-        availableMugs += 1
+            if self.shouldLogWeight(tmpWeight):
+                self._logger.info(
+                        "{0},{1}".format(datetime.utcnow().strftime("%Y-%m-%dT%X"), tmpWeight))
+                _currentWeight = tmpWeight
+                self.logToInitialState()
 
-    return availableMugs
-        
-def main(args):
-    rotateMinutes = timedelta(minutes = args.logRotateTimeMinutes)
-    rotateTime = datetime.utcnow() + rotateMinutes
-    global _currentWeight 
-    global _loopCount
-    _currentWeight = getWeightInGrams()
+            if shouldPostToLed():
+                _loopCount = 0
+                self.postToLed()
 
-    while True:
-        _loopCount += 1
-        tmpWeight = getWeightInGrams()
-        if datetime.utcnow() > rotateTime:
-            moveLogsToArchive(args.tempFile, args.permanentDirectory)
-            rotateTime = datetime.utcnow() + rotateMinutes
+            # if shouldPostToHipChat():
+            #     _loopCount = 0
+            #     writeToHipChat()
 
-        if shouldLogWeight(tmpWeight):
-            logger.info("{0},{1}".format(datetime.utcnow().strftime("%Y-%m-%dT%X"), tmpWeight))
-            _currentWeight = tmpWeight
-            logToInitialState()
-
-        if shouldPostToLed():
-            _loopCount = 0
-            postToLed()
-
-        # if shouldPostToHipChat():
-        #     _loopCount = 0
-        #     writeToHipChat()
-
-        sleep(1)
-
+            sleep(1)
 
 if __name__ == "__main__":
-    main(args)
-
+    scale = CoffeeScale()
+    parser = scale.getParser()
+    args = parser.parse_args()
+    scale.configureLogFile(args)
+    scale.main(args)
--- test_coffee_scale.py
+++ test_coffee_scale.py
@@ -3,7 +3,8 @@ import coffee_scale as cs
 
 class CoffeeTest(unittest.TestCase):
     def setUp(self):
-        self.parser = cs.getParser()
+        self.scale = cs.CoffeeScale()
+        self.parser = self.scale.getParser()
 
     def test_empty_args_throws_exception(self):
         with self.assertRaises(SystemExit):
@@ -17,85 +18,89 @@ class CoffeeTest(unittest.TestCase):
         self.assertEquals(args.logRotateTimeMinutes, 1)
 
     def test_whenValueChangesLessThanThreshold_LogIsNotWritten(self):
-        cs._currentWeight = 20
-        cs._threshold = 5
-        shouldLogItem = cs.shouldLogWeight(21)
+        self.scale._currentWeight = 20
+        self.scale._threshold = 5
+        shouldLogItem = self.scale.shouldLogWeight(21)
         self.assertEquals(False, shouldLogItem)
 
     def test_whenValueChangesGreaterThanThreshold_LogIsWritten(self):
-        cs._currentWeight = 1
-        cs._threshold = 5
-        shouldLogItem = cs.shouldLogWeight(7)
+        self.scale._currentWeight = 1
+        self.scale._threshold = 5
+        shouldLogItem = self.scale.shouldLogWeight(7)
         self.assertEquals(True, shouldLogItem)
 
     def test_whenValueIsAtLowThreshold_PotHasBeenLifted(self):
-        cs._lowThreshold = 10
-        cs._currentWeight = 0
-        potIsLifted = cs.potIsLifted()
+        potIsLifted = self.scale.potIsLifted()
         self.assertEquals(True, potIsLifted)
 
     def test_whenLoopedConfiguredTimes_PostMessageToHipchat(self):
-        cs._loopCount = 40
-        self.assertEquals(True, cs.shouldPostToHipChat())
+        self.scale._loopCount = 40
+        self.assertEquals(True, self.scale.shouldPostToHipChat())
 
     def test_whenLoopCountNotEqualConfiguredTimes_MessageNotPostedToHipChat(self):
         for i in range(40):
-            cs._loopCount = i
-            self.assertEquals(False, cs.shouldPostToHipChat())
+            self.scale._loopCount = i
+            self.assertEquals(False, self.scale.shouldPostToHipChat())
 
     def test_hipchatUserIsGiven_WithAMultipleOfNumberOfMugsInPot(self):
-        cs._currentWeight = 2600
-        self.assertEquals(6, cs.getAvailableMugs())
+        self.scale._currentWeight = 2600
+        self.assertEquals(6, self.scale.getAvailableMugs())
 
-        cs._currentWeight = 2264
-        self.assertEquals(5, cs.getAvailableMugs())
+        self.scale._currentWeight = 2264
+        self.assertEquals(5, self.scale.getAvailableMugs())
 
-        cs._currentWeight = 1999
-        self.assertEquals(4, cs.getAvailableMugs(), "{0} did not equal 4 mugs".format(cs._currentWeight))
+        self.scale._currentWeight = 1999
+        self.assertEquals(4, self.scale.getAvailableMugs(), "{0} did not equal 4 mugs".format(self.scale._currentWeight))
 
-        cs._currentWeight = 1733
-        self.assertEquals(3, cs.getAvailableMugs())
+        self.scale._currentWeight = 1733
+        self.assertEquals(3, self.scale.getAvailableMugs())
 
-        cs._currentWeight = 1467
-        self.assertEquals(2, cs.getAvailableMugs())
+        self.scale._currentWeight = 1467
+        self.assertEquals(2, self.scale.getAvailableMugs())
 
-        cs._currentWeight = 1201
-        self.assertEquals(1, cs.getAvailableMugs())
+        self.scale._currentWeight = 1201
+        self.assertEquals(1, self.scale.getAvailableMugs())
 
     def test_whenWeightWithin90PercentMinus10GramsOfFullMug_RegisterNextAvailableMug(self):
-        cs._currentWeight = 1164
-        self.assertEquals(1, cs.getAvailableMugs())
+        self.scale._currentWeight = 1164
+        self.assertEquals(1, self.scale.getAvailableMugs())
 
-        cs._currentWeight = 1430
-        self.assertEquals(2, cs.getAvailableMugs())
+        self.scale._currentWeight = 1430
+        self.assertEquals(2, self.scale.getAvailableMugs())
 
-        cs._currentWeight = 1696
-        self.assertEquals(3, cs.getAvailableMugs())
+        self.scale._currentWeight = 1696
+        self.assertEquals(3, self.scale.getAvailableMugs())
 
-        cs._currentWeight = 1962
-        self.assertEquals(4, cs.getAvailableMugs())
+        self.scale._currentWeight = 1962
+        self.assertEquals(4, self.scale.getAvailableMugs())
 
-        cs._currentWeight = 2228
-        self.assertEquals(5, cs.getAvailableMugs())
+        self.scale._currentWeight = 2228
+        self.assertEquals(5, self.scale.getAvailableMugs())
 
     def test_hipChatMessage_IncludesNumberOfMugs_AndWeightOfPot(self):
-        cs._currentWeight = 1173
-        params = cs.getHipchatParameters()
-        totalAvailableMugs = len(cs._mugAmounts)
+        self.scale._currentWeight = 1173
+        params = self.scale.getHipchatParameters()
+        totalAvailableMugs = len(self.scale._mugAmounts)
         self.assertEquals("{0} / {1}".format(
-            cs.getAvailableMugs(), totalAvailableMugs), params['from'])
+            self.scale.getAvailableMugs(), totalAvailableMugs), params['from'])
 
         self.assertEquals("{0} / {1}".format(
-            cs._currentWeight, cs._mugAmounts[totalAvailableMugs - 1]), 
+            self.scale._currentWeight, self.scale._mugAmounts[totalAvailableMugs - 1]), 
             params['message'])
+
+    def test_environmentVariables_AreSet(self):
+        self.assertTrue(self.scale.initialStateKey)
+        self.assertTrue(self.scale.hipchatKey)
+        self.assertTrue(self.scale.environment)
+        self.assertTrue(self.scale.ledServiceUrl)
 
 
     @unittest.skip("Only run manually if testing Initial State Integration")
     def test_InitialStateIntegration(self):
         for j in range(5):
             for i in range(5, 30, 2):
-                cs._currentWeight = i
-                cs.logToInitialState()
+                self.scale._currentWeight = i
+                self.scale.logToInitialState()
 
 def main():
     unittest.main()
