95fddcfb6a097a2d4d8513b6d2b3e03e7020a82b
James Alexander <jalexander@leafsoftwaresolutions.com>
2015-08-11 13:21:52 -0400

    - Hack - Adding one to the current PID to get the PID for the python process for monit to use for stopping the python process

--- bin/startcoffee.sh
+++ bin/startcoffee.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . /root/env.sh
-echo $$ > /var/run/coffee.pid
+echo $(($$ + 1)) > /var/run/coffee.pid
 /root/coffee-scale/venv/bin/python /root/coffee-scale/src/coffee_scale.py 1>/var/log/coffee 2>&1
